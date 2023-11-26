fb68ef9c90735bb88259e585d31a8c9e8d4091f0
Tyler Van Gorder <tkvangorder@users.noreply.github.com>
2023-11-26 13:41:08 -0800

    Orange is the theme?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,10 +18,10 @@ export default function Home() {
         />
       </div>
       <div className="mt-10 flex items-center justify-center gap-x-6">
-      <Link href="/poker" className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+      <Link href="/poker" className="rounded-md bg-orange-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
             Login
         </Link>
-        <Link href="/poker" className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+        <Link href="/poker" className="rounded-md bg-orange-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
             Join the Club
         </Link>
       </div>
